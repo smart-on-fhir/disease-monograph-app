b39e9e8542aeeb2a1f04d6e5a316aa8d766bbcdf
Nikolai Schwertner <nikolai.schwertner@childrens.harvard.edu>
2014-08-20 10:06:11 -0400

    Update sample rest app

--- rest/sample_app/app.py
+++ rest/sample_app/app.py
@@ -2,16 +2,16 @@
 #    [ ] encrypt the state to protect app secrets
 #    [ ] add logic to trim the session object when it gets too big before werkzeug crashes instead of
 #        clearing the session object on each run
-#    [ ] enable client secret between auth server and sa
+#    [ ] enable client secret between auth server and sample app
+#    [ ] add intermediary 'authorize' page to handle the transition to final page
+#    [ ] add configuration options and suggestions for session storage mechanism
 
-import pickle
 from flask import Flask, request, redirect, session, url_for
 from client import FHIRClient
 
 settings = {
     'app_id': 'my_web_app',
     'scope':  'patient/*.read',
-    'security_mode': None,
     'secret': ''
 }
 
@@ -21,60 +21,53 @@ app.secret_key = 'khsathdnsthjre'  # CHANGE ME
 
 @app.route('/fhir-app/launch.html')
 def launch():
-    s = settings
-    api_base = None
-    security_mode = None
     iss = request.args.get('iss', '')
     fhirServiceUrl = request.args.get('fhirServiceUrl', '')
-    launch_token = request.args.get('launch', '')
-    app_url = request.url.split('/fhir-app')[0] + url_for('index')
-    
+
     if iss:
-        # OAuth required
         api_base = iss
         security_mode = 'oauth'
     elif fhirServiceUrl:
-        # no authorization required
         api_base = fhirServiceUrl
+        security_mode = None
     
-    client = FHIRClient (app_id=s['app_id'], app_url=app_url, api_base=api_base,
-                 scope=s['scope'], launch_token=launch_token, 
-                 security_mode=security_mode, secret=s['secret'])
+    client = FHIRClient(app_id=settings['app_id'], 
+                 app_url=request.url.split('/fhir-app')[0] + url_for('authorize'),
+                 api_base=api_base,
+                 scope=settings['scope'],
+                 launch_token=request.args.get('launch', ''), 
+                 security_mode=security_mode,
+                 secret=settings['secret'])
 
-    session.clear()
-    state_id, state = client.state
-    session[state_id] = state
+    session['client_state'] = client.state
     
-    if client.authorize_url:
-        return redirect(client.authorize_url)
-    else:
-        return redirect(app_url + "?state=" + state_id)
+    return redirect(client.authorize_url)
 
+@app.route('/fhir-app/authorize.html')
+def authorize():
+    client = FHIRClient(state=session['client_state'], secret=settings['secret'])
+    client.update_access_token(request.args.get('code', ''))
+    session['client_state'] = client.state
+    return redirect(url_for('index'))
+    
 @app.route('/fhir-app/')
 def index():
-    authorization_code = request.args.get('code', '')
-    state_id = request.args.get('state', '')
-
-    client = FHIRClient (state=session[state_id])
-    if not client.access_token and authorization_code:
-        client.update_access_token (authorization_code)
-        
-    session.clear()
-    state_id, state = client.state
-    session[state_id] = state
-
+    client = FHIRClient(state=session['client_state'], secret=settings['secret'])
+    patient = client.Patient()
+    prescriptions = client.MedicationPrescription()
+    
     out = """<!DOCTYPE html>
         <html>
           <head><title>Sample REST App</title></head>
           <body>
     """
     
-    patient = client.Patient()
-    name = patient['name'][0]['given'][0] + " " + patient['name'][0]['family'][0] + " " + patient['birthDate']
-    out += "<h1>Medications for <span id='name'>%s</span></h1>\n" % name
+    h = ' '.join((patient['name'][0]['given'][0], patient['name'][0]['family'][0], patient['birthDate']))
+    out += "<h1>Medications for <span id='name'>%s</span></h1>\n" % h
     out += "<ul id='med_list'>\n"
     
-    prescriptions = client.MedicationPrescription()
+    # medication may only be present as a reference in the prescription -> in a complete implementation
+    # would need to implement a more flexible/adaptive mechanism for retrieval
     
     for prescription in prescriptions:
         meds = prescription['contained']
